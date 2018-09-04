b5818701f45fa23af3155cd630ff7eeaafa1cfc5
Nemanja Ristic <rista.pirot@gmail.com>
2018-09-04 15:50:55 +0200

    search bar working now

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <v-content>
     <section>
-      <v-parallax src="https://images.pexels.com/photos/1095814/pexels-photo-1095814.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" height="700">
+      <v-parallax src="https://images.pexels.com/photos/37728/pexels-photo-37728.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" height="700">
           <v-layout
             column
             align-center
@@ -31,6 +31,7 @@
         <v-flex xs12 sm4 class="my-3">
           <div class="text-xs-center">
             <h2 class="display-2">See how it works!</h2>
+            <h2 class="mt-3">Choose loading and unloading country!</h2>
           </div>
         </v-flex>
         <v-flex xs12>
@@ -50,10 +51,12 @@
                     cache-items
                     class="mx-2"
                     flat
+                    clearable
                     hide-no-data
                     hide-details
                     label="Select loading place?"
                     solo-inverted
+                    @change="searchPlace"
                   ></v-autocomplete>
                 </v-toolbar>
               </v-flex>
@@ -71,10 +74,12 @@
                     cache-items
                     class="mx-2"
                     flat
+                    clearable
                     hide-no-data
                     hide-details
                     label="Select unloading place?"
                     solo-inverted
+                    @change="searchPlace"
                   ></v-autocomplete>
                 </v-toolbar>
               </v-flex>
@@ -87,7 +92,7 @@
               <v-flex xs12 md6>
                 <v-expansion-panel>
                 <v-expansion-panel-content
-                  v-for="(fleet,i) in fleets"
+                  v-for="(fleet,i) in newFleets"
                   :key="i"
                 >
                   <div slot="header" class="title"><span class="blue--text headline lighten-1 text-capitalize font-weight-medium">{{ fleet.startCountry }}</span>, {{ fleet.startCity }} - <span class="blue--text font-weight-medium headline text-capitalize lighten-1">{{ fleet.endCountry }}</span>, {{ fleet.endCity }}</div>
@@ -166,19 +171,6 @@
               </v-layout>
             </v-container>
           </v-flex>
-          <!-- <v-layout align-center justify-center class="white--text display-1 mt-5 mb-3">
-            <v-flex xs12 style="line-height: 1.5em;">
-              <h2>You need to login to see fleet details! It is <span class="white--text">FREE</span>!</h2>
-            </v-flex>
-          </v-layout> -->
-          <!-- <v-layout 
-            align-start
-            justify-center
-            class="white--text loginRegister pt-4"
-          >
-            
-            
-          </v-layout> -->
         </v-layout>
     </section>
 
@@ -200,7 +192,7 @@
                 <v-icon class="blue--text text--lighten-2">phone</v-icon>
               </v-list-tile-action>
               <v-list-tile-content>
-                <v-list-tile-title class="white--text">+38166450066 - Nemanja</v-list-tile-title>
+                <v-list-tile-title class="white--text">+38166450066 - Nemanja, CEO</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
             <v-list-tile>
@@ -230,6 +222,7 @@ export default {
       loading2: false,
       items: [],
       items2: [],
+      newFleets: [],
       fleets: [
         {
           startCountry: 'Serbia',
@@ -243,10 +236,32 @@ export default {
           weight: 24
         },
         {
-          startCountry: 'Serbia',
-          startCity: 'Belgrade',
+          startCountry: 'Bulgaria',
+          startCity: 'Blagoevgrad',
+          endCountry: 'Serbia',
+          endCity: 'Celarevo',
+          price: 700,
+          startDate: '06.09.2018.',
+          person: 'Igor Jonic',
+          phone: '+38162450022',
+          weight: 22
+        },
+        {
+          startCountry: 'Germany',
+          startCity: 'Berlin',
           endCountry: 'Bulgaria',
           endCity: 'Sofia',
+          price: 2100,
+          startDate: '12.09.2018.',
+          person: 'John Carter',
+          phone: '+3875561516',
+          weight: 20
+        },
+        {
+          startCountry: 'Serbia',
+          startCity: 'Belgrade',
+          endCountry: 'Bosnia',
+          endCity: 'Sarajevo',
           price: 500,
           startDate: '05.09.2018.',
           person: 'Milos Simonovic',
@@ -254,33 +269,11 @@ export default {
           weight: 24
         },
         {
-          startCountry: 'Serbia',
-          startCity: 'Belgrade',
-          endCountry: 'Bulgaria',
-          endCity: 'Sofia',
-          price: 500,
-          startDate: '05.09.2018.',
-          person: 'Milos Simonovic',
-          phone: '+38162656667',
-          weight: 24
-        },
-        {
-          startCountry: 'Serbia',
-          startCity: 'Belgrade',
-          endCountry: 'Bulgaria',
-          endCity: 'Sofia',
-          price: 500,
-          startDate: '05.09.2018.',
-          person: 'Milos Simonovic',
-          phone: '+38162656667',
-          weight: 24
-        },
-        {
-          startCountry: 'Serbia',
-          startCity: 'Belgrade',
-          endCountry: 'Bulgaria',
-          endCity: 'Sofia',
-          price: 500,
+          startCountry: 'Bulgaria',
+          startCity: 'Ruse',
+          endCountry: 'Serbia',
+          endCity: 'Novi Sad',
+          price: 1000,
           startDate: '05.09.2018.',
           person: 'Milos Simonovic',
           phone: '+38162656667',
@@ -343,6 +336,29 @@ export default {
         })
         this.loading = false
       }, 500)
+    },
+    searchPlace () {
+      if (this.select) {
+        this.newFleets = this.fleets.filter(el => el.startCountry === this.select)
+      }
+
+      if (this.select2) {
+        this.newFleets = this.fleets.filter(el => el.endCountry === this.select2)
+      }
+
+      if (this.select && this.select2) {
+        this.newFleets = this.fleets.filter(el => el.startCountry === this.select && el.endCountry === this.select2)
+      }
+
+      // if (this.select !== null && this.select2 !== '') {
+      //   this.newFleets = this.fleets.filter(el => el.startCountry === this.select && el.endCountry === this.select2)
+      // } else if (this.select !== '' && (this.select2 == null || this.select2 == undefined)) {
+      //   this.newFleets = this.fleets.filter(el => el.startCountry === this.select)
+      // } else if ((this.select === null || this.select === undefined) && this.select2 !== '') {
+      //   this.newFleets = this.fleets.filter(el => el.endCountry === this.select2)
+      // } else {
+      //   this.newFleets = this.fleets
+      // }
     }
   }
 }
